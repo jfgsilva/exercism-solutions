c011fed0a3b3a9edc748a2113076ab14a0fc7460
João Filipe Silva <jfcgsilva@oney.pt>
2022-11-30 15:00:38 +0000

    fixed tree_building

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,7 +1,7 @@
 package tree
 
 import (
-	"fmt"
+	"errors"
 	"sort"
 )
 
@@ -24,28 +24,29 @@ func Build(records []Record) (*Node, error) {
 	// every record is a node
 	nodes := make([]Node, len(records))
 	rootNode := &nodes[0]
-	fmt.Println("records:", records)
-	for _, record := range records {
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+	for index, record := range records {
 		switch {
+		case record.ID != index:
+			return nil, errors.New("record ID doesn't match index: duplicate")
+		case record.ID != 0 && record.ID == record.Parent:
+			return nil, errors.New("direct cycle: only root can be it's own parent")
+		case record.ID < record.Parent:
+			return nil, errors.New("record id cannot be higher than parent id")
 		// this is the root node
 		// if childer is not nil the children node already populated children slice, and we would erase
+		case record.ID == 0 && record.Parent != 0:
+			return nil, errors.New("root node can't have parent")
 		case record.ID == 0 && record.Parent == 0 && nodes[0].Children == nil:
-			nodes[record.ID] = Node{ID: record.ID}
-			// fmt.Println("here's the root node:", record)
-			// fmt.Println(nodes[record.ID])
-		case record.ID != 00:
-			nodes[record.ID] = Node{ID: record.ID}
-			// this seems correct, however it seems that something is missing in creating a relation between different nesting levels
+			nodes[record.ID].ID = record.ID
+		case record.ID != 00 && index == 0:
+			return nil, errors.New("single record which isn't root")
+		case record.ID != 00 && index != 0:
+			nodes[record.ID].ID = record.ID
 			nodes[record.Parent].Children = append(nodes[record.Parent].Children, &nodes[record.ID])
-			// after appending we need to reorder the children
 		}
-		// the children slice needs to be sorted
-		children := nodes[record.Parent].Children
-		sort.Slice(children, func(i, j int) bool {
-			return children[i].ID < children[j].ID
-		})
-
 	}
-	fmt.Println("nodes:", nodes)
 	return rootNode, nil
 }
