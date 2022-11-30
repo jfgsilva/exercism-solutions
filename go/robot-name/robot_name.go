ceb07932c72f6dc13fd00dc73c376cb690ac6a4e
João Filipe Silva <jfcgsilva@oney.pt>
2022-11-30 19:47:41 +0000

    still got collision error

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -2,60 +2,70 @@ package robotname
 
 import (
 	"errors"
-	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
 // Define the Robot type here.
 type Robot struct {
-	name string
+	name        string
+	isNameTaken bool
 }
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	generator()
 }
 
 // maxNumberNames = possible combinations of 2 letters + 3 digits
-const maxNumberNames = 26 * 26 * 10 * 10 * 10
+const (
+	maxNumberNames = 26 * 26 * 10 * 10 * 10
+)
 
-// Cnt is increased each time a key is stored in DB
-var Cnt = 0
+// cnt is increased each time a key is stored in db
+var cnt = 0
+var letters = strings.Split("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "")
+var digits = strings.Split("0123456789", "")
 
 // map pre created with max length to hold the robot names so we can check if they exist
-var DB = make(map[string]int, maxNumberNames)
+var db = make(map[int]Robot, maxNumberNames)
 
-var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+// anonymous func to generated the robot's name
+func generator() {
+	cnt2 := 0
+	for _, l1 := range letters {
+		for _, l2 := range letters {
+			for _, d1 := range digits {
+				for _, d2 := range digits {
+					for _, d3 := range digits {
+						name := l1 + l2 + d1 + d2 + d3
+						db[cnt2] = Robot{name: name, isNameTaken: false}
+						cnt2 += 1
+					}
+				}
+			}
+		}
+	}
+}
 
 func (r *Robot) Name() (string, error) {
 	// the name is assigned so no need to generate it: we just return the robot's name
 	if r.name != "" {
 		return r.name, nil
 	}
-	// anonymous func to generated the robot's name
-	generator := func() string {
-		// generates two letters
-		ltrs := make([]rune, 2)
-		for i := range ltrs {
-			ltrs[i] = letters[rand.Intn(len(letters))]
-		}
-		///generates three digits
-		tripe_digit := fmt.Sprintf("%03d", rand.Intn(1000))
-		return string(ltrs) + tripe_digit
-	}
 	// infite loop
 	for {
-		key := generator()
-		if Cnt > maxNumberNames {
+		if cnt >= maxNumberNames {
 			// we ran through all possible values
 			return "", errors.New("max Number of different names reached")
 		}
-		// we check if
-		if _, ok := DB[key]; !ok {
-			DB[key] = 1
-			Cnt += 1
-			r.name = key
-			return key, nil
+		randRobot := rand.Intn(maxNumberNames)
+		if !db[randRobot].isNameTaken {
+			nr := db[randRobot]
+			nr.isNameTaken = true
+			r.name = db[randRobot].name
+			return r.name, nil
 		} else {
 			continue
 		}
